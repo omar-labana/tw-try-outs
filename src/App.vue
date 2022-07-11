b814a21ecd88340410696994cbb0bfc6c5ac1ded
Omar Abd Elhameed <omar.abdelhameed@scandiweb.com>
2022-07-11 22:37:48 +0200

    add font and initial color scheme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="py-2 px-3 text-white">Hello, World!</h1>
+  <h1 class="bg-base-fill py-2 px-3 text-3xl text-base-text">Hello, World!</h1>
+  <h1 class="bg-inverted-fill py-2 px-3 text-3xl text-inverted-text">
+    Hello, World!
+  </h1>
 </template>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "base-fill": "#1D1D1D",
+      "inverted-fill": "#F8F8F8",
+      "base-text": "#F4F4F4",
+      "inverted-text": "#1D1D1D",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Averta CY", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
